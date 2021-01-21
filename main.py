afd5f80c3f833562a34dc034ec21b5ba17f2ac02
Тилек Токтокожоев <tilektoktokozoev@MacBook-Air-Tilek.local>
2021-01-21 20:23:05 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+x = 10
+y = 120
+z = 120
+c = 10
+x = z
+y = c
+print(x)
+print(y)
+price = input("How much the price of laptop in dollars US?")
+print("Well,", price, "$")
+month = input("So for how many month i can pay maximum?")
+print("Good, so for every month it will be" , int(price) / int(month) * 84, "som. Think, i can buy it")
